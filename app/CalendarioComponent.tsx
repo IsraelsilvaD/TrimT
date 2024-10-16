ae07ca2e43b53daeac89b746b2903519a2ae0248
Renata Gonzaga <79698768+Renata-Gonzaga@users.noreply.github.com>
2024-10-16 17:37:27 +0100

    Update CalendarioComponent.tsx

--- app/CalendarioComponent.tsx
+++ app/CalendarioComponent.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView, ImageBackground } from 'react-native';
-import { Calendar, LocaleConfig } from 'react-native-calendars'; // Importando o componente de calendário
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 import moment from 'moment';
 
-// Definindo um tipo para os horários disponíveis
 type AvailableTimes = string[];
 
-// Configurando o calendário para exibir os meses e dias em português
 LocaleConfig.locales['pt'] = {
   monthNames: [
     'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
@@ -17,10 +15,9 @@ LocaleConfig.locales['pt'] = {
     'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
   ],
   dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
   today: 'Hoje'
 };
-
 LocaleConfig.defaultLocale = 'pt';
 
 const CalendarioComponent = () => {
@@ -28,140 +25,170 @@ const CalendarioComponent = () => {
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
   const [availableTimes, setAvailableTimes] = useState<AvailableTimes>([]);
 
-  // Simular horários disponíveis para a data selecionada
   const handleSelectDate = (date: string) => {
     setSelectedDate(date);
-    setAvailableTimes(['2024-10-10T09:00:00','2024-10-10T10:00:00', '2024-10-10T11:00:00', '2024-10-10T12:00:00','2024-10-10T14:00:00','2024-10-10T15:00:00','2024-10-10T16:00:00','2024-10-10T17:00:00','2024-10-10T18:00:00']);
+    setAvailableTimes([
+      '2024-10-10T09:00:00', '2024-10-10T10:00:00', '2024-10-10T11:00:00', 
+      '2024-10-10T12:00:00', '2024-10-10T14:00:00', '2024-10-10T15:00:00', 
+      '2024-10-10T16:00:00', '2024-10-10T17:00:00', '2024-10-10T18:00:00'
+    ]);
   };
 
   const handleSelectTime = (time: string) => {
-    setSelectedTime(time === selectedTime ? null : time); // Alterna a seleção da hora
+    setSelectedTime(time === selectedTime ? null : time);
   };
 
   return (
-    <ImageBackground 
-      source={require('../assets/images/imgFundo.jpeg')} // Substitua pelo caminho da sua imagem
-      style={styles.background}
+    <ImageBackground
+      source={require('../assets/images/imgFundo.jpeg')}
+      style={styles.background}      
     >
       <View style={styles.content}>
         <View style={styles.calendarContainer}>
           <Text style={styles.monthView}>Calendário</Text>
           <Calendar
-            onDayPress={(day: { dateString: string }) => handleSelectDate(day.dateString)} // Definindo o tipo do parâmetro day
-            markedDates={{
-              [selectedDate || '']: { selected: true, selectedColor: '#FFD700' }, // Usando selectedDate com fallback para string vazia
-            }}
+            onDayPress={(day: { dateString: string }) => handleSelectDate(day.dateString)}
+            markedDates={{ [selectedDate || '']: { selected: true, selectedColor: '#FFD700' } }}
             style={styles.calendar}
+            theme={{
+              calendarBackground: '#141109',
+              textSectionTitleColor: '#FFFFFF',
+              dayTextColor: '#ffffff',
+              selectedDayBackgroundColor: '#FFD700',
+              selectedDayTextColor: '#000000',
+              todayTextColor: '#FFFFFF',
+              arrowColor: '#FFFFFF',
+              monthTextColor: '#FFFFFF',
+              indicatorColor: '#FFD700',
+              textDayFontFamily: 'monospace',
+              textMonthFontFamily: 'monospace',
+              textDayHeaderFontFamily: 'monospace',
+              textDayFontWeight: '300',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 14
+            }}
           />
-        </View>
-
-        {selectedDate && (
-          <View style={styles.timeSlots}>
-            <Text style={styles.timeSlotButton}>
-              Horários disponíveis para {moment(selectedDate).format('LL')}
-            </Text>
-            <ScrollView style={styles.scrollView}>
-              {availableTimes.map((time, index) => (
-                <TouchableOpacity 
-                  key={index} 
-                  style={[
-                    styles.timeSlotButton,
-                    selectedTime === time && styles.selectedTimeSlot // Aplica estilo se a hora estiver selecionada
-                  ]}
-                  onPress={() => handleSelectTime(time)} // Seleciona a hora ao clicar
-                >
-                  <Text style={styles.timeText}>{moment(time).format('HH:mm')}</Text> {/* Aplicando estilo ao texto da hora */}
+          {selectedDate && (
+            <>
+              <Text style={styles.timeSlotHeader}>
+                Horários disponíveis para {moment(selectedDate).format('LL')}
+              </Text>
+              <ScrollView style={styles.scrollView} horizontal>
+                {availableTimes.map((time, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    style={[
+                      styles.timeSlotButton,
+                      selectedTime === time && styles.selectedTimeSlot
+                    ]}
+                    onPress={() => handleSelectTime(time)}
+                  >
+                    <Text style={styles.timeText}>{moment(time).format('HH:mm')}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.cancelButton}>
+                  <Text style={styles.buttonText}>Cancelar</Text>
                 </TouchableOpacity>
-              ))}
-            </ScrollView>
-
-            {/* Botões de Agendar e Cancelar */}
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.cancelButton}>
-                <Text style={{ color: 'white' }}>Cancelar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.confirmButton}>
-                <Text style={{ color: 'white' }}>Agendar</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
+                <TouchableOpacity style={styles.confirmButton}>
+                  <Text style={styles.buttonText}>Agendar</Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
+        </View>
       </View>
     </ImageBackground>
   );
 };
 
-// Estilos adaptados para React Native
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     resizeMode: 'cover', // Ajusta a imagem para cobrir o fundo
   },
   content: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    paddingTop: 20,
-    width: '100%',
-    marginLeft: 50,
-    justifyContent: 'flex-start',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
   calendarContainer: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
     width: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)', // Fundo cinza transparente
+    backgroundColor: '#141109',
     padding: 20,
-    marginRight: 50, // Desloca o calendário para a direita
+    borderRadius: 10,
+    alignItems: 'center',
   },
   monthView: {
-    backgroundColor: '#2c2c2c',
+    fontSize: 20,
     color: 'white',
-    padding: 10,
+    marginBottom: 10,
   },
   calendar: {
     width: '100%',
-    height: 350, // Ajuste a altura conforme necessário
+    height: 350,
   },
   timeSlots: {
-    flex: 1,
-    borderRadius: 4,
     marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
+  },
+  timeSlotHeader: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 10,
   },
   scrollView: {
-    maxHeight: 200, // Define a altura máxima para a barra de rolagem
+    maxHeight: 100,
+    paddingHorizontal: 10,
   },
   timeSlotButton: {
-    flex: 1,
     padding: 10,
     borderRadius: 4,
     fontSize: 16,
-    marginRight: 5,
+    marginHorizontal: 5,
     backgroundColor: '#2c2c2c',
     color: 'white',
+    minWidth: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   selectedTimeSlot: {
-    backgroundColor: '#FFD700', // Cor amarela quando selecionado
+    backgroundColor: '#FFD700',
   },
   timeText: {
-    color: 'white', // Texto da hora em branco
-    textAlign: 'center', // Centraliza o texto
+    color: 'white',
+    textAlign: 'center',
   },
   confirmButton: {
     backgroundColor: '#28a745',
     padding: 10,
     borderRadius: 4,
-    marginRight: 5,
+    marginHorizontal: 5,
   },
   cancelButton: {
     backgroundColor: '#dc3545',
     padding: 10,
     borderRadius: 4,
-    marginRight: 5,
+    marginHorizontal: 5,
   },
   buttonContainer: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 10,
   },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
+
 
 export default CalendarioComponent;
